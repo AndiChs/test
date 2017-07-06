280a21e282bd71e4d445e00e15e039ded14f5943
ConstantineLamar <andichesoi@gmail.com>
2017-07-06 04:23:58 +0200

    User Persist Data'

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,31 @@
-<h1>Is working</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <table class="table">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+                <th>Role</th>
+            </tr>
+            </thead>
+            <tbody>
+            @if($users)
+                @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                    <td>{{$user->role->name}}</td>
+                </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+@stop
